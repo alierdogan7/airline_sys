8d1fff224382769b57f2ae98a8e99eea90f3d196
alierdogan7 <erdoganaliburak@gmail.com>
2016-04-20 12:00:40 +0300

    new models added (few models left)

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Question)
+admin.site.register(City)
+admin.site.register(Airport)
 admin.site.register(Customer)
 admin.site.register(Staff)
-admin.site.register(Airport)
-admin.site.register(City)
+admin.site.register(Manager)
+admin.site.register(Salesman)
+admin.site.register(Plane)
+admin.site.register(Seat)
+admin.site.register(Crew)
+admin.site.register(Pilot)
+admin.site.register(Hostess)
+admin.site.register(FlightLeg)
+admin.site.register(Reservation)
+
--- polls/models.py
+++ polls/models.py
@@ -23,7 +23,7 @@ class Choice(models.Model):
 
 class City(models.Model):
 	city_name = models.CharField(max_length=50, primary_key=True)
-	no_of_airports = models.IntegerField(default=0)
+	no_of_airports = models.PositiveSmallIntegerField(default=0)
 
 	def __str__(self):
 		return self.city_name
@@ -32,8 +32,8 @@ class Airport(models.Model):
 	airport_code = models.CharField(max_length=20, primary_key=True)
 	airport_name = models.CharField(max_length=100, null=False)
 	supports_flight_legs = models.BooleanField(default=False)
-	max_airplanes = models.IntegerField(null=False)
-	city_name = models.ForeignKey(City, on_delete=models.CASCADE)
+	max_airplanes = models.PositiveSmallIntegerField(null=False)
+	city_name = models.ForeignKey(City, on_delete=models.CASCADE, db_column='city_name')
 
 	def __str__(self):
 		return "%s (%s)" % (self.airport_name, self.airport_code)
@@ -45,8 +45,8 @@ class Customer(models.Model):
 	email = models.EmailField(null=False)
 	phone = models.CharField(max_length=20, null=False)
 	last_login = models.DateTimeField('last login', null=True)
-	no_of_flights = models.IntegerField(default=0)
-	lives_in = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+	no_of_flights = models.PositiveSmallIntegerField(default=0)
+	lives_in = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, db_column='lives_in')
 
 	def __str__(self):
 		return self.fullname
@@ -59,12 +59,115 @@ class Staff(models.Model):
 	phone = models.CharField(max_length=20, null=False)
 	last_login = models.DateTimeField('last login', null=True)
 	salary = models.IntegerField(default=0)
-	manager_id = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
-	works_in = models.ForeignKey(Airport, on_delete=models.CASCADE)
+	manager_id = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, db_column='manager_id')
+	works_in = models.ForeignKey(Airport, on_delete=models.CASCADE, db_column='works_in')
 
 	def __str__(self):
 		return self.fullname
 
-# class Manager(models.Model):
-# 	staff_id = models.ForeignKey(Staff, primary_key=True)
-# 	degree = models.CharField(max_length=100)
+class Manager(models.Model):
+ 	staff_id = models.ForeignKey(Staff, primary_key=True, db_column='staff_id')
+
+ 	DEGREE = (
+ 		("full", "full authorization"),
+ 		("partial", "partial authorization"),
+ 	)
+ 	degree = models.CharField(max_length=100, null=False, choices=DEGREE)
+
+class Salesman(models.Model):
+ 	staff_id = models.ForeignKey(Staff, primary_key=True, db_column='staff_id')
+ 	no_of_sold_tickets = models.PositiveSmallIntegerField(default=0)
+
+class Plane(models.Model):
+	plane_id = models.AutoField(primary_key=True)
+	model = models.CharField(max_length=100, null=False)
+	production_year = models.CharField(max_length=4, null=False)
+	max_seats = models.PositiveSmallIntegerField(null=False)
+
+	def __str__(self):
+		return "%s (ID: %s)" % (self.model, self.plane_id)
+
+	def gen_seats(self, num_of_seats, seats_per_row):
+		#seat_list = []
+		if num_of_seats % seats_per_row != 0:
+			print "invalid options"
+			return	
+		else:
+			for number in xrange(1, num_of_seats / seats_per_row + 1):
+				for letter in xrange(ord('A'), ord('A') + seats_per_row):
+					#seat_list.append( (number, chr(letter)) )
+					self.seat_set.create(seat_number=number, seat_letter=chr(letter))
+
+
+
+class Seat(models.Model):
+	seat_number = models.PositiveSmallIntegerField(null=False)
+	seat_letter = models.CharField(max_length=1, null=False)
+	plane_id = models.ForeignKey(Plane, on_delete=models.CASCADE, db_column='plane_id', null=False)
+
+	SEAT_CLASSES = (
+			('business', 'Business Class'),
+			('economy', 'Economy Class'),
+	)
+	seat_class = models.CharField(max_length=20, null=False, choices=SEAT_CLASSES)
+
+	class Meta:
+		unique_together = ("seat_letter", "seat_number", "plane_id")
+
+	def __str__(self):
+		return "%d%s" % (self.seat_number, self.seat_letter)
+		
+
+class Crew(models.Model):
+ 	staff_id = models.ForeignKey(Staff, primary_key=True, db_column='staff_id')
+	num_of_flights = models.PositiveSmallIntegerField(default=0)
+	since = models.DateField(auto_now_add=True)
+	participates = models.ManyToManyField('FlightLeg')
+
+
+class Pilot(models.Model):
+ 	staff_id = models.ForeignKey(Staff, primary_key=True, db_column='staff_id')
+
+	LICENSE_TYPES = (
+			("national", "only in country"),
+			("international", "international flights"),
+	)
+	license_type = models.CharField(max_length=100, null=False, choices=LICENSE_TYPES)
+
+class Hostess(models.Model):
+ 	staff_id = models.ForeignKey(Staff, primary_key=True, db_column='staff_id')
+	mother_language = models.CharField(max_length=30)
+	first_aid_ability = models.BooleanField(default=False)
+
+class FlightLeg(models.Model):
+	flight_leg_code = models.CharField(max_length=7, null=False)
+	time = models.DateTimeField(null=False)
+	estimated_arr_time = models.DateTimeField(null=False)
+	arrives = models.ForeignKey(Airport, related_name='landing_flights', on_delete=models.CASCADE, db_column='arrives', null=False)
+	departs = models.ForeignKey(Airport, related_name='departing_flights', on_delete=models.CASCADE, db_column='departs', null=False)
+	plane_id = models.ForeignKey(Plane, on_delete=models.CASCADE, db_column='plane_id', null=False)
+	no_of_available_seats = models.PositiveSmallIntegerField(null=False)
+	is_cancelled = models.BooleanField(default=False)
+	travel_distance = models.IntegerField(null=False)
+	price_for_economy = models.IntegerField(null=False)
+	price_for_business = models.IntegerField(null=False)
+
+	class Meta:
+		unique_together = ("flight_leg_code", "time")
+
+	def __str__(self):
+		return "%s (Departs: %s)" % (self.flight_leg_code, self.time)
+		
+
+class Reservation(models.Model):
+	reservation_code = models.CharField(max_length=6, primary_key=True)
+	cust_id = models.ForeignKey(Customer, db_column='cust_id', null=False)
+	fligt_leg = models.ForeignKey(FlightLeg, null=False)
+	seat = models.ForeignKey(Seat, null=False)
+	sold_by = models.ForeignKey(Salesman, null=True, blank=True)
+	extra_luggage = models.BooleanField(default=False)
+	reservation_time = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return "%s" % (self.reservation_code)
+		
